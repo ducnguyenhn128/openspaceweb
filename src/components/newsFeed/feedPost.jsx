d8e1561376a1e2df63fabf3e29640ccae6c3fc65
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2023-05-23 19:50:41 +0700

    Update feedPost.jsx

--- src/components/newsFeed/feedPost.jsx
+++ src/components/newsFeed/feedPost.jsx
@@ -17,6 +17,7 @@ const FeedPost = (props) => {
     const time = createdAt && typeof createdAt === 'string' ? createdAt.slice(0, 10) : '';
     const author_page = '/user/' + author;
     const post_link = '/post/' + _id;
+    const formattedBody = body.replace(/\n/g, "<br>");
     return (  
         <div className="text-start bg-white p-3 mb-3 border border-white rounded">
             {/* Author && Time && More Setting */}
@@ -44,7 +45,8 @@ const FeedPost = (props) => {
             </div>
             {/* Body */}
             <div className="mt-4">
-                <p><pre>{body}</pre></p>
+                {/* body: enter in new line */}
+                <p dangerouslySetInnerHTML={{ __html: formattedBody }}></p>
                 {/* <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.</p> */}
             </div>
 
